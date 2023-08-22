8c75df51e49de426696cc427ba5cb9978b85f70f
Nishchal Verma <42401969+Nishchal5@users.noreply.github.com>
2023-08-22 22:49:30 +0530

    QR Code Generator

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import QRCode from "react-qr-code";
 
 function App() {
+  const [text, setText] = useState("www.linkedin.com/in/nishchal5");
+  const textChangeHanlder = (event) => {
+    setText(event.target.value);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="mainDiv">
+      <h1>QR Code Generator</h1>
+      <input
+        type="text"
+        value={text}
+        onChange={textChangeHanlder}
+        placeholder="Enter you text here to generate QR"
+      />
+      <div
+        style={{
+          height: "auto",
+          margin: "0 auto",
+          maxWidth: 200,
+          width: "100%"
+        }}
+      >
+        <QRCode
+          size={256}
+          style={{ height: "auto", maxWidth: "100%", width: "100%" }}
+          value={text}
+          viewBox={`0 0 256 256`}
+        />
+      </div>
     </div>
   );
 }
